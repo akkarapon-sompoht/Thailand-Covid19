92756e62973d08124a84e76e45c696a2a087ad78
tong <akkarapon.somp61@sskru.ac.th>
2021-01-08 21:21:53 +0700

    first commit

--- lib/get_api.dart
+++ lib/get_api.dart
@@ -5,104 +5,79 @@ import 'package:http/http.dart' as Http;
 
 class GetAPI extends StatefulWidget {
   GetAPI({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
   _GetAPIState createState() => _GetAPIState();
 }
 
-
 class _GetAPIState extends State<GetAPI> {
   var jsonData;
   Map<String, int> data = {};
 
+  final keys = [
+    'confirmed',
+    'recovered',
+    'hospitalized',
+    'newConfirmed',
+  ];
+
   Future<String> _GetCovid19Today() async {
-    var response = await Http.get('https://covid19.th-stat.com/api/open/today');
-    jsonData = jsonDecode(utf8.decode(response.bodyBytes));
-    print(jsonData);
+    var respone = await Http.get('https://covid19.th-stat.com/api/open/today');
+    jsonData = jsonDecode(utf8.decode(respone.bodyBytes));
 
     data['confirmed'] = jsonData['Confirmed'];
     data['recovered'] = jsonData['Recovered'];
-    data['hospitalized'] = jsonData['Hospitalized']; 
+    data['hospitalized'] = jsonData['Hospitalized'];
     data['newConfirmed'] = jsonData['NewConfirmed'];
 
-    return "OK";
-
+    return "Ok";
   }
 
-  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Thai Covid19 Today'),
+        title: Text('สถิติทั่วไป'),
       ),
       body: Center(
-        child: FutureBuilder(
-          future: _GetCovid19Today(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return new Container(
-                child: new Column(
-                  children: <Widget>[
-                    Text('NewConfirmed'),
-                    Text('${data['newConfirmed']}'),
-                    SizedBox(
-                      width: 120,
-                      height: 50,
-                    ),
-                     Text('Recovered'),
-                     Text('${data['recovered']}'),
-                      SizedBox(
-                      width: 120,
-                      height: 50,
-                    ),
-                    Text('Confirmed'),
-                     Text('${data['confirmed']}'),
-                      SizedBox(
-                      width: 120,
-                      height: 50,
-                    ),
-                    Text('Hospitalized'),
-                     Text('${data['hospitalized']}'),
-                      SizedBox(
-                      width: 120,
-                      height: 50,
-                    ),
-                  ],
-                ),
-              );
-   body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-              child: Column(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Center(
-                      child: Text(
-                      ),
-                    )
-                  )
-                  SizedBox(height: sizeBox),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(200.0),
-                    child: Image.asset(
-                      'assets/images/covid19.jpg',
-                      height: 180.0,
-                    ),
+          child: new FutureBuilder(
+              future: _GetCovid19Today(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          color: Colors.orange[100],
+                          elevation: 1,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 10.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: new Container(
+                            padding: new EdgeInsets.all(32.0),
+                            child: new Column(
+                              children: <Widget>[
+                                new Text(
+                                  '${keys[index]}',
+                                ),
+                                new Text(
+                                  '${data[keys[index]]}',
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
                   );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),   
-              );
-            }
-          },
-        ),
-      ),
+                }
+              })),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
